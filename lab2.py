be604c1ebd3d4d2abb55f89db691d45f990d18f2
hugeyikers <147138078+hugeyikers@users.noreply.github.com>
2024-03-13 12:52:22 +0100

    Add files via upload

--- lab2.py
+++ lab2.py
@@ -1,86 +1,63 @@
-import sys as system
+import math
+import random
 
 def zadanie1():
-    zdanie = input('Podaj zdanie: ')
-    ilosc_slow = len(zdanie.split())
-    print(ilosc_slow)
+    a = (math.e**4 - math.log(34, 2))**(1/3)
+    print("Podpunkt a: ", round(a, 2))
+
+    b = (math.log(20, 10) + math.cos(45) + math.e)**(1/3)
+    print("Podpunkt b: ", round(b, 2))
+
+    c = (math.log(20, 3) + math.sin(45) * (5/8))**(1/4)
+    print("Podpunkt c: ", round(c, 2))
+
+    d = math.log(23, 2) + (math.sin(34) + 5)**(1/3)
+    print("Podpunkt d: ", round(d, 2))
+
+    e = (math.log(32, 2) + math.pi + math.sin(56))**(1/4)
+    print("Podpunkt e: ", round(e, 2))
+
 
 def zadanie2():
-    system.stdout.write("podaj 3 liczby calkowite: ")
-    a = int(system.stdin.readline())
-    b = int(system.stdin.readline())
-    c = int(system.stdin.readline())
-    x = a**b + c
-    system.stdout.write(str(x))
+    liczba_rzedow = int(input("Podaj liczbe rzedow dla wiezy: "))
+    if liczba_rzedow <= 10:
+        for i in range(liczba_rzedow+1):
+            a = 'A'*i
+            print(a)
+    else:
+        print("Podaj liczbe rzedow nie wieksza niz 10")
+
 
 def zadanie3():
-    a = input('Podaj slowo: ')
-    b = a[::-1]
-    if a == b:
-        print("palindrom")
-    else:
-        print('nie palindrom')
-
-def zadanie4():
-    a = int(input('Podaj liczbe: '))
-    if a/1==a and a/a==a:
-        print('liczba pierwsza')
-    elif a == 0:
-        print('nie liczba pierwsza')
-    else:
-        print('nie liczba pierwsza')
+    liczba_rzedow = int(input("Podaj liczbe rzedow dla wiezy: "))
+    if liczba_rzedow <= 10:
+        for i in range(liczba_rzedow + 1):
+            if i % 2 != 0:
+                a = ' A' * i
+                print(a.center(19))
+            elif i % 2 == 0:
+                a = ' A' * i
+                print(a.center(20))
+            else:
+                print("Podaj liczbe rzedow nie wieksza niz 10")
 
 def zadanie5():
-    lista = []
-    for i in range(1,1001):
-        counter = 0
-        for j in range(1,i):
-            if i % j == 0:
-                counter += j
-        if counter == i:
-            lista.append(i)
-    print(lista)
-
-def zadanie6():
-    lista = [2, 2.5, 3, 10, 15.1, 20.4]
-    lista2 = []
-    for i in lista:
-        lista2.append(i**2)
-    print(lista2)
-
-def zadanie7():
-    lista = []
-    counter = 0
-    while counter < 10:
-        a = int(input('Podaj liczbe: '))
-        if a%2==0:
-            lista.append(a)
-        counter += 1
-    print(lista)
-
-def zadanie8():
-    lista = [5, 'jablko', 'gruszka', 10, 'pomarancza', 3, 7]
-    slownik = {}
-    for i in lista:
-        slownik[i] = lista.count(i)
-    lista2 = []
-    print(slownik)
-    for i in slownik:
-        if isinstance(i,str):
-            lista2.append(i)
-    for i in lista2:
-        slownik.pop(i)
-    print(slownik)
+    n = int(input("Podaja ilosc wierszy i kolumn: "))
+    for i in range(n+1):
+        b = ''
+        c = 0
+        for j in range(n+1):
+            a = random.randint(0,9)
+            b += '   ' + str(a)
+            c += a
+        print(f"{b}     Suma wiersza wynosi {c}")
 
 
 
 def main():
-    #zadanie1()
-    #zadanie2()
-    #zadanie3()
-    #zadanie4()
-    #zadanie5()
-    #zadanie6()
-    #zadanie7()
-    zadanie8()
+    #print(zadanie1())
+    #print(zadanie2())
+    #print(zadanie3())
+    print(zadanie5())
+
 main()
